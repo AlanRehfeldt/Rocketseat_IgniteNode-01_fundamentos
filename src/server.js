2515c9648036f93db326cd056f528fa7ed1e833b
Alan Rehfeldt <alanrehfeldt@gmail.com>
2024-06-10 22:11:03 -0300

    conhecendo http status code

--- src/server.js
+++ src/server.js
@@ -5,8 +5,6 @@ const users = []
 const server = http.createServer((req, res) => {
   const { method, url } = req
 
-  console.log(method, url)
-
   if(method === 'GET' && url === '/users') {
     return res
       .setHeader('Content-type', 'application/json')
@@ -20,10 +18,10 @@ const server = http.createServer((req, res) => {
       email: 'johndoe@example.com'
     })
 
-    return res.end('Criação de usuários')
+    return res.writeHead(201).end()
   }
 
-  return res.end('Hello Ignite')
+  return res.writeHead(404).end()
 })
 
 server.listen(3333)
